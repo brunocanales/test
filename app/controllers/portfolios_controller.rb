1dc4b9852f5609ff6dffde728c46fa12b9d7e6c7
brunocanales <brunocanales@yahoo.com>
2018-09-25 16:51:07 -0300

    Implmented edit and update functionlity

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,5 +5,43 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def new
+
+    @portfolio_item = Portfolio.new
+
+  end
+
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+  respond_to do |format|
+    if @portfolio_item.save
+      format.html { redirect_to portfolios_path, notice: 'Your Post is now live.' }
+
+    else
+      format.html { render :new }
+
+    end
+  end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+        format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 end
