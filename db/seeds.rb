6c536d3ed40fbea5feb7a022a1a61409610a2a78
brunocanales <brunocanales@yahoo.com>
2018-09-25 15:12:54 -0300

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |x|
+  Blog.create!(
+
+          title: "My blog Post #{x}",
+          body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+  )
+end
+
+puts "10 posts creados"
+
+5.times do |i|
+
+  Skill.create!(
+
+
+
+                    title: "Rails #{i}",
+                    percent_utilized: 15
+           )
+
+  end
+
+  puts "5 skills creados"
+
+
+  9.times do |portfolio_items|
+
+    Portfolio.create!(
+                 title: "Portgfolio Item #{portfolio_items}",
+                 subtitle: "My grat Service",
+                 body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                 main_image: "https://via.placeholder.com/600x400",
+                 thumb_image:"https://via.placeholder.com/350x200"
+
+
+    )
+  end
